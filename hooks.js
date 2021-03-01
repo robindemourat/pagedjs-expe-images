d7b6e87f9f3cc98b6588b14c0539bc80e3159968
Robin de Mourat <robin.demourat@gmail.com>
2021-03-01 18:14:29 +0100

    add first attempt for multiply

--- hooks.js
+++ hooks.js
@@ -3,27 +3,41 @@ const replaceImage = (domImage, transformFn) => {
     const canvas = document.createElement('canvas');
     let ctx = canvas.getContext("2d");
     domImage.crossOrigin = "anonymous";  // This enables CORS
-    domImage.addEventListener('load', function() {
-			const displayedWidth = domImage.getBoundingClientRect().width;
-      const displayedHeight = domImage.getBoundingClientRect().height;
-			
-      canvas.width = displayedWidth;
-      canvas.height = displayedHeight;
+    let isLoaded = false;
+		domImage.addEventListener('load', function() {
+			if (!isLoaded) {
+				const displayedWidth = domImage.getBoundingClientRect().width;
+				const displayedHeight = domImage.getBoundingClientRect().height;
+				
+				canvas.width = displayedWidth;
+				canvas.height = displayedHeight;
 
-      if (typeof transformFn === 'function') {
-        ctx = transformFn(ctx, domImage, displayedWidth, displayedHeight);
-      } else {
-        ctx.drawImage(domImage, 0, 0, displayedWidth, displayedHeight);
-      }
-      domImage.src = canvas.toDataURL();
-      resolve(domImage);
+				if (typeof transformFn === 'function') {
+					ctx = transformFn(ctx, domImage, displayedWidth, displayedHeight);
+				} else {
+					ctx.drawImage(domImage, 0, 0, displayedWidth, displayedHeight);
+				}
+				domImage.src = canvas.toDataURL();
+				resolve(domImage);
+				isLoaded = true;
+			}
+			
     })
     
     domImage.addEventListener('error', reject);
   })
 }
 const parseImages = (transformFn) => {
-  Promise.all([...document.querySelectorAll('img')].map(img => replaceImage(img, transformFn)))
+	const images = document.querySelectorAll('img');
+
+  Promise.all(
+		[...images].map(
+			img => replaceImage(img, transformFn)
+		)
+	)
+	.then(() => {
+		console.log('toutes les images ont été transformées')
+	})
 }
 
 const blackAndWhite = (ctx, image, displayedWidth, displayedHeight) => {
@@ -34,13 +48,24 @@ const blackAndWhite = (ctx, image, displayedWidth, displayedHeight) => {
   ctx.fillRect(0, 0, displayedWidth, displayedHeight);
   return ctx;
 }
+const applyBlendMode = mode => (ctx, image, displayedWidth, displayedHeight) => {
+	ctx.globalAlpha = 0.5;
+	// ctx.drawImage(image, 0, 0, displayedWidth, displayedHeight);
+	ctx.globalCompositeOperation = mode;
+	
+	ctx.drawImage(image, 0, 0, displayedWidth, displayedHeight);
+	ctx.globalAlpha = 0;
+	ctx.fillRect(0, 0, displayedWidth / 2, displayedHeight);
+
+  return ctx;
+}
 class MyHandler extends Paged.Handler {
 		constructor(chunker, polisher, caller) {
 			super(chunker, polisher, caller);
 		}
 
 		afterRendered(_pages) {
-			parseImages(blackAndWhite);
+			parseImages(applyBlendMode('multiply'));
 		}
 	}
 	Paged.registerHandlers(MyHandler);
